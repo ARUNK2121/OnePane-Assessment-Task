b1905f947d935f2762a2518b7c905395d68326d5
Arun K <arthurbishop120@gmail.com>
2023-12-29 23:28:52 +0530

    fetching process made concurrent operations

--- handler/handler.go
+++ handler/handler.go
@@ -4,56 +4,58 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	database "one_pane_assessment/Database"
+	database "one_pane_assessment/database"
 	"one_pane_assessment/helper"
 	"one_pane_assessment/models"
+	"sync"
 )
 
 type Handler struct {
-	Posts    []models.Post
-	Comments []models.Comment
-	Users    []models.User
+	Posts    chan models.Post
+	Comments chan models.Comment
+	Users    chan models.User
 }
 
 func NewHandler() *Handler {
 	return &Handler{
-		Posts:    make([]models.Post, 0),
-		Comments: make([]models.Comment, 0),
-		Users:    make([]models.User, 0),
+		Posts:    make(chan models.Post),
+		Comments: make(chan models.Comment),
+		Users:    make(chan models.User),
 	}
 }
 
 func (h *Handler) HandleRoute(w http.ResponseWriter, r *http.Request) {
 	db := database.NewDatabase()
-	var err error
-	h.Posts, err = helper.FetchPosts()
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error fetching posts: %v", err), http.StatusInternalServerError)
-		return
-	}
-	h.Comments, err = helper.FetchComments()
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error fetching comments: %v", err), http.StatusInternalServerError)
-		return
-	}
-	h.Users, err = helper.FetchUsers()
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error fetching users: %v", err), http.StatusInternalServerError)
-		return
-	}
+	var errorChan chan error
 
-	// Store to map for easy manipulation and better time complexity for fetching (in worst case O(n))
-	for _, v := range h.Users {
-		db.Users[int(v.Id)] = v
-	}
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+	go func(c <-chan models.User, wg *sync.WaitGroup) {
+		defer wg.Done()
+		for v := range h.Users {
+			db.Users[int(v.Id)] = v
+		}
+	}(h.Users, &wg)
 
-	for _, v := range h.Comments {
-		db.Comments[int(v.PostID)]++
-	}
+	go func(c <-chan models.Comment, wg *sync.WaitGroup) {
+		defer wg.Done()
+		for v := range h.Comments {
+			db.Comments[int(v.PostID)]++
+		}
+	}(h.Comments, &wg)
 
-	for _, v := range h.Posts {
-		db.Posts[int(v.ID)] = v
-	}
+	go func(c <-chan models.Post, wg *sync.WaitGroup) {
+		defer wg.Done()
+		for v := range h.Posts {
+			db.Posts[int(v.ID)] = v
+		}
+	}(h.Posts, &wg)
+
+	go helper.FetchPosts(h.Posts, errorChan)
+	go helper.FetchComments(h.Comments, errorChan)
+	go helper.FetchUsers(h.Users, errorChan)
+
+	wg.Wait()
 
 	var Result []models.Result
 	for i, v := range db.Posts {
--- helper/helper.go
+++ helper/helper.go
@@ -7,62 +7,74 @@ import (
 	"one_pane_assessment/models"
 )
 
-func FetchPosts() ([]models.Post, error) {
+func FetchPosts(c chan<- models.Post, errorChan chan error) {
 	res, err := http.Get("https://jsonplaceholder.typicode.com/posts")
 	if err != nil {
-		return nil, err
+		errorChan <- err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed to fetch posts. status code: %d", res.StatusCode)
+		errorChan <- fmt.Errorf("failed to fetch posts. status code: %d", res.StatusCode)
 	}
 
 	var posts []models.Post
 	err = json.NewDecoder(res.Body).Decode(&posts)
 	if err != nil {
-		return nil, err
+		errorChan <- err
 	}
 
-	return posts, nil
+	for _, v := range posts {
+		v := v
+		c <- v
+	}
+	close(c)
 }
 
-func FetchComments() ([]models.Comment, error) {
+func FetchComments(c chan<- models.Comment, errorChan chan error) {
 	res, err := http.Get("https://jsonplaceholder.typicode.com/comments")
 	if err != nil {
-		return nil, err
+		errorChan <- err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed to fetch comments. status code: %d", res.StatusCode)
+		errorChan <- fmt.Errorf("failed to fetch comments. status code: %d", res.StatusCode)
 	}
 
 	var comments []models.Comment
 	err = json.NewDecoder(res.Body).Decode(&comments)
 	if err != nil {
-		return nil, err
+		errorChan <- err
 	}
 
-	return comments, nil
+	for _, v := range comments {
+		v := v
+		c <- v
+	}
+	close(c)
 }
 
-func FetchUsers() ([]models.User, error) {
+func FetchUsers(c chan<- models.User, errorChan chan error) {
 	res, err := http.Get("https://jsonplaceholder.typicode.com/users")
 	if err != nil {
-		return nil, err
+		errorChan <- err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed to fetch users. status code: %d", res.StatusCode)
+		errorChan <- fmt.Errorf("failed to fetch users. status code: %d", res.StatusCode)
 	}
 
 	var users []models.User
 	err = json.NewDecoder(res.Body).Decode(&users)
 	if err != nil {
-		return nil, err
+		errorChan <- err
 	}
 
-	return users, nil
+	for _, v := range users {
+		v := v
+		c <- v
+	}
+	close(c)
 }
