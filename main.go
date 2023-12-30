7463de21f2217248f5e115507ae3973a2fed8d47
Arun K <arthurbishop120@gmail.com>
2023-12-30 12:30:26 +0530

    just some refactorings

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"one_pane_assessment/handler"
 )
@@ -8,5 +10,9 @@ import (
 func main() {
 	h := handler.NewHandler()
 	http.HandleFunc("/", h.HandleRoute)
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("server running on port:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("server failed to start")
+	}
 }
